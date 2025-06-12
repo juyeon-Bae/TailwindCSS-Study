a1d6c26331cefe6c6828cede67b22fbfbfb7e813
juyeonBae <w2316@e-mirim.hs.kr>
2025-06-12 12:07:23 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Test from "./pages/test";
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Test />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/test.jsx
@@ -0,0 +1,8 @@
+import tailwindConfig from "../../tailwind.config"
+export default function Test() {
+    return (
+        <div className="text-3xl pt-1 text-clifford font-bold underline">
+            Hello This is tailwind css!
+        </div>
+    )
+}
